008710ea8a2202a4a00c89cdb1984e5c01fdab46
Siming Chen <siming.chen@sap.com>
2018-07-18 16:01:13 +0800

    persistence data

--- Readme.md
+++ Readme.md
@@ -4,6 +4,14 @@
 ## commond
 
 ## run go chain 
+
 `cd goblockchain`
+
 `go build`
+
 `./goblockchain`
+
+## ref
+
+[blockchain](https://github.com/Jeiwan/blockchain_go)
+[doc](https://jeiwan.cc/posts/building-blockchain-in-go-part-1/)
--- block.go
+++ block.go
@@ -1,6 +1,11 @@
 package main
 
-import "time"
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+	"time"
+)
 
 //Block is
 type Block struct {
@@ -26,4 +31,28 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 //NewGenesisBlock is
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
